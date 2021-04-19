ecd3e837c0c32ab93d6dabc4be3637bb28c4b70c
Masum-Web-Developer <masum.diucse@gmail.com>
2021-04-20 00:24:00 +0600

    addAdminEmail,addPackage,addReview,getAllPackage,getAllReviews,checkAdmin api created

--- index.js
+++ index.js
@@ -1,7 +1,74 @@
 var express = require('express');
 var app = express();
+const MongoClient = require('mongodb').MongoClient;
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv').config()
+app.use(cors());
+app.use(bodyParser.json());
 
-app.get('/',(req,res)=>{
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kdxcg.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+client.connect(err => {
+    const adminCollection = client.db(`${process.env.DB_NAME}`).collection(`${process.env.DB_COLLECTION}`);
+    const packageCollection = client.db(`${process.env.DB_NAME}`).collection(`${process.env.DB_COLLECTION_2}`);
+    const reviewCollection = client.db(`${process.env.DB_NAME}`).collection(`${process.env.DB_COLLECTION_3}`);
+
+    //--add admin email
+    app.post('/addAdminEmail', (req, res) => {
+        const email = req.body;
+        adminCollection.insertOne(email)
+            .then(result => {
+                res.send(result.insertedCount > 0)
+            })
+    })
+    //-add package
+    app.post('/addPackage', (req, res) => {
+        const package = req.body;
+        packageCollection.insertOne(package)
+            .then(result => {
+                res.send(result.insertedCount > 0)
+            })
+    })
+
+      //-add review
+      app.post('/addReview', (req, res) => {
+        const review = req.body;
+        reviewCollection.insertOne(review)
+            .then(result => {
+                res.send(result.insertedCount > 0)
+            })
+    })
+
+    //--get all package
+    app.get('/getAllPackage', (req, res) => {
+        packageCollection.find({})
+            .toArray((err, packages) => {
+                res.send(packages);
+            })
+    })
+
+      //--get all testimonial
+      app.get('/getAllReviews', (req, res) => {
+        reviewCollection.find({})
+            .toArray((err, reviews) => {
+                res.send(reviews);
+            })
+    })
+    //--check admin
+    app.post('/checkAdmin', (req, res) => {
+        const data = req.body;
+        adminCollection.find(data)
+          .toArray((err, email) => {
+            res.send(email.length>0);
+          })
+      })
+    console.log("DB Connected");
+
+});
+
+app.get('/', (req, res) => {
     res.send("Hello from clean-moto-server;!!");
 })
 
